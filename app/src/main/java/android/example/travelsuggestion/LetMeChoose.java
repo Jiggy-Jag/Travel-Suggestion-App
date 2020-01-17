0fa5673c6011333bd2abf45595e2c454a61e9590
FarrukhGit <57110408+FarrukhGit@users.noreply.github.com>
2020-01-17 21:30:24 +0000

    Add files via upload
    first commit: Bubbles

---
+++ app/src/main/java/android/example/travelsuggestion/LetMeChoose.java
@@ -0,0 +1,73 @@
+package android.example.travelsuggestion;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.igalata.bubblepicker.BubblePickerListener;
+import com.igalata.bubblepicker.model.PickerItem;
+import com.igalata.bubblepicker.rendering.BubblePicker;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+
+public class LetMeChoose extends AppCompatActivity {
+
+    BubblePicker bubblePicker;
+    String[] name={
+            "New York", "Dubai", "Sydney", "Morocco", "New Orleans",
+            "France", "Maldives"
+    };
+
+    int[] images={
+            R.drawable.newyork,
+            R.drawable.dubai,
+            R.drawable.sydney,
+            R.drawable.morocco,
+            R.drawable.neworleans,
+            R.drawable.france,
+            R.drawable.maldives
+    };
+
+    int[] colors={
+            Color.parseColor("#1A237E"),
+            Color.parseColor("#6200EA"),
+            Color.parseColor("#004D40"),
+            Color.parseColor("#880E4F"),
+            Color.parseColor("#B71C1C"),
+            Color.parseColor("#5200EA"),
+            Color.parseColor("#C04D40"),
+    };
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_let_me_choose);
+
+        bubblePicker= (BubblePicker) findViewById(R.id.picker);
+        ArrayList<PickerItem> listItems=new ArrayList<>();
+        for(int i=0;i<name.length;i++){
+            PickerItem item=new PickerItem(name[i],colors[i], Color.WHITE, getDrawable(images[i]));
+            listItems.add(item);
+        }
+
+        bubblePicker.setItems(listItems);
+        bubblePicker.setListener(new BubblePickerListener() {
+            @Override
+            public void onBubbleSelected(@NotNull PickerItem pickerItem) {
+                Toast.makeText(LetMeChoose.this,""+pickerItem.getTitle()+" Selected",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onBubbleDeselected(@NotNull PickerItem pickerItem) {
+                Toast.makeText(LetMeChoose.this,""+pickerItem.getTitle()+" Deselected",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
