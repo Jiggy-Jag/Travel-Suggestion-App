b8527084339f996e1eac337d84db006ee642f596
vithursha <56385991+vithursha@users.noreply.github.com>
2020-01-29 23:07:41 +0000

    interactive map

--- app/src/main/java/android/example/travelsuggestion/helpMePick.java
+++ app/src/main/java/android/example/travelsuggestion/helpMePick.java
@@ -1,14 +1,56 @@
 package android.example.travelsuggestion;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class helpMePick extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class helpMePick extends FragmentActivity implements OnMapReadyCallback {
+
+
+    public void LosAngelesInfo(View view){
+        Intent intent = new Intent(this,los_angeles_info.class);
+        startActivity(intent);
+    }
+
+
+    GoogleMap map;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_me_pick);
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        map = googleMap;
+        // Add a marker in LA and move the camera
+        LatLng LosAngeles = new LatLng(34.053164, -118.252480);
+        map.addMarker(new MarkerOptions().position(LosAngeles).title("Marker in Los Angeles"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(LosAngeles));
+
+        LatLng London = new LatLng(51.508531, -0.129322);
+        map.addMarker(new MarkerOptions().position(London).title("Marker in London"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(London));
+
+
     }
 }
