97b980d070beab49e331ebebc1b0cb851170fd00
Prabhleen <prabhleen_chana@hotmail.co.uk>
2020-01-30 14:40:25 +0000

    fixed login button

--- app/src/main/java/android/example/travelsuggestion/LoginScreen.java
+++ app/src/main/java/android/example/travelsuggestion/LoginScreen.java
@@ -1,6 +1,8 @@
 package android.example.travelsuggestion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,11 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+    }
+
+    public void Login(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
