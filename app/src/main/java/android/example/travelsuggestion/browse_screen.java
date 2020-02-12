e530964e827844a80b54051bae23bbeb11d78c2f
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2020-02-12 18:38:19 +0000

    adding destination info template

--- app/src/main/java/android/example/travelsuggestion/browse_screen.java
+++ app/src/main/java/android/example/travelsuggestion/browse_screen.java
@@ -3,22 +3,33 @@ package android.example.travelsuggestion;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class browse_screen extends AppCompatActivity {
+public static final String EXTRA_TEXT = "com.example.application.example.EXTRA_TEXT";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_screen);
     }
-    public void LosAngelesInfo(View view){
-        Intent intent = new Intent(this,los_angeles_info.class);
+
+    public void Open_los_angeles_info(View view){
+        int destination = 1;
+        Intent intent = new Intent(this,info_template.class);
+        intent.putExtra(EXTRA_TEXT,destination);
         startActivity(intent);
+
     }
+
     public void Vancouverinfo(View view) {
-        Intent intent = new Intent(this, vancouverinfo.class);
+        int destination = 2;
+        Intent intent = new Intent(this, info_template.class);
+        intent.putExtra(EXTRA_TEXT,destination);
         startActivity(intent);
     }
 
