99396157d53240019bf297dbbf4ecd82c2f96e06
KonradDlugosz <56347935+KonradDlugosz@users.noreply.github.com>
2020-02-12 17:38:38 +0000

    added info template layout

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/activity/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/activity/R.java
@@ -154,13 +154,13 @@ public final class R {
     public static final class layout {
         private layout() {}
 
-        public static final int custom_dialog = 0x7f0a0023;
-        public static final int notification_action = 0x7f0a0024;
-        public static final int notification_action_tombstone = 0x7f0a0025;
-        public static final int notification_template_custom_big = 0x7f0a0026;
-        public static final int notification_template_icon_group = 0x7f0a0027;
-        public static final int notification_template_part_chronometer = 0x7f0a0028;
-        public static final int notification_template_part_time = 0x7f0a0029;
+        public static final int custom_dialog = 0x7f0a0024;
+        public static final int notification_action = 0x7f0a0025;
+        public static final int notification_action_tombstone = 0x7f0a0026;
+        public static final int notification_template_custom_big = 0x7f0a0027;
+        public static final int notification_template_icon_group = 0x7f0a0028;
+        public static final int notification_template_part_chronometer = 0x7f0a0029;
+        public static final int notification_template_part_time = 0x7f0a002a;
     }
     public static final class string {
         private string() {}
