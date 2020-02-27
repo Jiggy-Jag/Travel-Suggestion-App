ba40cc5b47ca08a3ae69c1d4fc5131b1b582feba
FarrukhGit <57110408+FarrukhGit@users.noreply.github.com>
2020-02-27 10:05:13 +0000

    git ignore fix

--- app/src/main/java/android/example/travelsuggestion/http_test.java
+++ app/src/main/java/android/example/travelsuggestion/http_test.java
@@ -2,25 +2,42 @@ package android.example.travelsuggestion;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
-
+import androidx.appcompat.app.AppCompatActivity;
+import org.json.JSONException;
+import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class http_test extends AppCompatActivity {
 
     private TextView mResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.http_layout);
         mResult = findViewById(R.id.tv);
-        new GetDataTask().execute("http://172.31.82.136:3000/destinations/");
+
+        //make GET request
+        new GetDataTask().execute("http://172.31.82.136:3000/destinations/:id/1");
+
+        // make POST request
+        //new PostDataTask().execute("http://172.31.82.136:3000/");
+
+        //make PUT request
+        //new PutDataTask().execute("http://172.31.82.136:3000/destinations/");
+
+        //make DELETE request
+        // new DeleteDataTask().execute("http://172.31.82.136:3000/destinations/1");
+
     }
 
     class GetDataTask extends AsyncTask<String, Void, String> {
@@ -32,17 +49,20 @@ public class http_test extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... params) {
+
             try {
                 return getData(params[0]);
             } catch (IOException ex) {
                 return "Network error !";
             }
         }
+
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             mResult.setText(result);
         }
+
         private String getData(String urlPath) throws IOException {
             StringBuilder result = new StringBuilder();
             BufferedReader bufferedReader =null;
@@ -55,6 +75,74 @@ public class http_test extends AppCompatActivity {
                 urlConnection.setRequestMethod("GET");
                 urlConnection.setRequestProperty("Content-Type", "application/json");
                 urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+
+            } finally {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            }
+
+            return result.toString();
+        }
+    }
+
+    class PostDataTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                return postData(params[0]);
+            } catch (IOException ex) {
+                return "Network error !";
+            } catch (JSONException ex) {
+                return "Data Invalid !";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mResult.setText(result);
+        }
+
+        private String postData(String urlPath) throws IOException, JSONException {
+
+            StringBuilder result = new StringBuilder();
+            BufferedWriter bufferedWriter = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                JSONObject dataToSend = new JSONObject();
+                dataToSend.put("value1", "value2");
+                dataToSend.put("value", "value3");
+
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
                 InputStream inputStream = urlConnection.getInputStream();
                 bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String line;
@@ -65,7 +153,121 @@ public class http_test extends AppCompatActivity {
                 if (bufferedReader != null) {
                     bufferedReader.close();
                 }
+                if (bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
             }
+
             return result.toString();
-        }}
+        }
+    }
+
+    class PutDataTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return putData(params[0]);
+            } catch (IOException ex) {
+                return "Network error !";
+            } catch (JSONException ex) {
+                return "Data invalid !";
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mResult.setText(result);
+        }
+
+        private String putData(String urlPath) throws IOException, JSONException {
+
+            BufferedWriter bufferedWriter = null;
+            String result = null;
+
+            try {
+                //Create data to update
+                JSONObject dataToSend = new JSONObject();
+                dataToSend.put("username", "first-name");
+                dataToSend.put("content", "content");
+                dataToSend.put("name", 1);
+                dataToSend.put("comments", 2);
+
+                //Initialize and config request, then connect to server
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setDoOutput(true);
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataToSend.toString());
+                bufferedWriter.flush();
+
+                if (urlConnection.getResponseCode() == 200) {
+                    return "Update successfully !";
+                } else {
+                    return "Update failed !";
+                }
+            } finally {
+                if(bufferedWriter != null) {
+                    bufferedWriter.close();
+                }
+            }
+        }
+    }
+
+    class DeleteDataTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return deleteData(params[0]);
+            } catch (IOException ex) {
+                return "Network error !";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mResult.setText(result);
+        }
+
+        private String deleteData(String urlPath) throws IOException {
+
+            String result =  null;
+            URL url = new URL(urlPath);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(10000);
+            urlConnection.setRequestMethod("DELETE");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            urlConnection.connect();
+
+            if (urlConnection.getResponseCode() == 204) {
+                result = "Delete successfully !";
+            } else {
+                result = "Delete failed !";
+            }
+            return result;
+        }
+    }
+
 }
