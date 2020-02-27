8ae4df116fe9029426cb53c87198b80b51b5a9e6
vithursha <56385991+vithursha@users.noreply.github.com>
2020-02-27 10:50:37 +0000

    browse page

--- app/src/main/java/android/example/travelsuggestion/MainActivity.java
+++ app/src/main/java/android/example/travelsuggestion/MainActivity.java
@@ -2,8 +2,10 @@ package android.example.travelsuggestion;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     private Button letMeChoose;
     private Button helpMePick;
     private Button Home;
+    private Button http;
+    private Button RateUs;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
         helpMePick = (Button) findViewById(R.id.btnHelpMePick);
         letMeChoose = (Button) findViewById(R.id.btnLetMeChoose);
+        http = (Button) findViewById(R.id.http);
+        RateUs = (Button) findViewById(R.id.btn_RateUs);
 
     }
 
@@ -40,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openBrowseActivity(View view) {
         Intent intent = new Intent(this, browse_screen.class);
+        startActivity(intent);
+
+    }
+
+
+    public void openRatingActivity (View view){
+        Intent intent = new Intent(this, Rating_bar.class);
+        startActivity(intent);
+    }
+
+    public void openHTTPActivity(View view) {
+        Intent intent = new Intent(this, http_test.class);
         startActivity(intent);
 
     }
