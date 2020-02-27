e1ae34779f4bdc152ace7883dc8876142a733d2d
Jiggy-Jag <56381845+Jiggy-Jag@users.noreply.github.com>
2020-02-27 18:30:10 +0000

    Linked bubblepicker with HTTP requests

---
+++ app/src/main/java/android/example/travelsuggestion/afterselection.java
@@ -0,0 +1,90 @@
+package android.example.travelsuggestion;
+
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import androidx.annotation.RequiresApi;
+
+
+public class afterselection extends selection {
+    private TextView mResult;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_afterselection);
+        mResult = findViewById(R.id.Results);
+
+        new afterselection.GetDataTask().execute("http://172.31.82.136:3000/destinations/" + test);
+
+    }
+
+    class GetDataTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                return getData(params[0]);
+            } catch (IOException | JSONException ex) {
+                return "Network error !";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            mResult.setText(result);
+        }
+
+
+        private String getData(String urlPath) throws IOException, JSONException {
+            StringBuilder result = new StringBuilder();
+            BufferedReader bufferedReader = null;
+
+
+            try {
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000);
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    result.append(line).append("\n");
+                }
+
+            } finally {
+                if (bufferedReader != null) {
+                    bufferedReader.close();
+                }
+            }
+
+
+            return result.toString();
+        }
+
+    }
+}
--- app/src/main/java/android/example/travelsuggestion/http_test.java
+++ app/src/main/java/android/example/travelsuggestion/http_test.java
@@ -2,12 +2,15 @@ package android.example.travelsuggestion;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import  java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -19,15 +22,17 @@ import java.net.URL;
 public class http_test extends AppCompatActivity {
 
     private TextView mResult;
+    private TextView dest_id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.http_layout);
         mResult = findViewById(R.id.tv);
+        dest_id = findViewById(R.id.editText);
 
         //make GET request
-        new GetDataTask().execute("http://172.31.82.136:3000/destinations");
+
 
         // make POST request
         //new PostDataTask().execute("http://172.31.82.136:3000/");
@@ -40,127 +45,137 @@ public class http_test extends AppCompatActivity {
 
     }
 
-    class GetDataTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            try {
-                return getData(params[0]);
-            } catch (IOException ex) {
-                return "Network error !";
-            }
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            mResult.setText(result);
-        }
-
-        private String getData(String urlPath) throws IOException {
-            StringBuilder result = new StringBuilder();
-            BufferedReader bufferedReader =null;
-
-            try {
-                URL url = new URL(urlPath);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000);
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setRequestMethod("GET");
-                urlConnection.setRequestProperty("Content-Type", "application/json");
-                urlConnection.connect();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result.append(line).append("\n");
-                }
-
-            } finally {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-            }
-
-            return result.toString();
-        }
+    public void Test(View view) {
+        //make GET request when button is pressed
+        new GetDataTask().execute("http://172.31.82.136:3000/destinations/" + dest_id.getText());
     }
 
-    class PostDataTask extends AsyncTask<String, Void, String> {
+        class GetDataTask extends AsyncTask<String, Void, String> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected String doInBackground(String... params) {
-
-            try {
-                return postData(params[0]);
-            } catch (IOException ex) {
-                return "Network error !";
-            } catch (JSONException ex) {
-                return "Data Invalid !";
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
             }
-        }
 
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-            mResult.setText(result);
-        }
+            @Override
+            protected String doInBackground(String... params) {
 
-        private String postData(String urlPath) throws IOException, JSONException {
-
-            StringBuilder result = new StringBuilder();
-            BufferedWriter bufferedWriter = null;
-            BufferedReader bufferedReader = null;
-
-            try {
-                JSONObject dataToSend = new JSONObject();
-                dataToSend.put("value1", "value2");
-                dataToSend.put("value", "value3");
-
-                URL url = new URL(urlPath);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setReadTimeout(10000);
-                urlConnection.setConnectTimeout(10000);
-                urlConnection.setRequestMethod("POST");
-                urlConnection.setDoOutput(true);
-                urlConnection.setRequestProperty("Content-Type", "application/json");
-                urlConnection.connect();
-
-                OutputStream outputStream = urlConnection.getOutputStream();
-                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-                bufferedWriter.write(dataToSend.toString());
-                bufferedWriter.flush();
-
-                InputStream inputStream = urlConnection.getInputStream();
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
-                    result.append(line).append("\n");
-                }
-            } finally {
-                if (bufferedReader != null) {
-                    bufferedReader.close();
-                }
-                if (bufferedWriter != null) {
-                    bufferedWriter.close();
+                try {
+                    return getData(params[0]);
+                } catch (IOException | JSONException ex) {
+                    return "Network error !";
                 }
             }
 
-            return result.toString();
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                mResult.setText(result);
+            }
+
+
+            private String getData(String urlPath) throws IOException, JSONException {
+                StringBuilder result = new StringBuilder();
+                BufferedReader bufferedReader = null;
+
+
+                try {
+                    URL url = new URL(urlPath);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setReadTimeout(10000);
+                    urlConnection.setConnectTimeout(10000);
+                    urlConnection.setRequestMethod("GET");
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.connect();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result.append(line).append("\n");
+                    }
+
+                } finally {
+                    if (bufferedReader != null) {
+                        bufferedReader.close();
+                    }
+                }
+
+
+                return result.toString();
+            }
+
         }
-    }
+
+        class PostDataTask extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(String... params) {
+
+                try {
+                    return postData(params[0]);
+                } catch (IOException ex) {
+                    return "Network error !";
+                } catch (JSONException ex) {
+                    return "Data Invalid !";
+                }
+            }
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                mResult.setText(result);
+            }
+
+            private String postData(String urlPath) throws IOException, JSONException {
+
+                StringBuilder result = new StringBuilder();
+                BufferedWriter bufferedWriter = null;
+                BufferedReader bufferedReader = null;
+
+                try {
+                    JSONObject dataToSend = new JSONObject();
+                    dataToSend.put("value1", "value2");
+                    dataToSend.put("value", "value3");
+
+                    URL url = new URL(urlPath);
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    urlConnection.setReadTimeout(10000);
+                    urlConnection.setConnectTimeout(10000);
+                    urlConnection.setRequestMethod("POST");
+                    urlConnection.setDoOutput(true);
+                    urlConnection.setRequestProperty("Content-Type", "application/json");
+                    urlConnection.connect();
+
+                    OutputStream outputStream = urlConnection.getOutputStream();
+                    bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                    bufferedWriter.write(dataToSend.toString());
+                    bufferedWriter.flush();
+
+                    InputStream inputStream = urlConnection.getInputStream();
+                    bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        result.append(line).append("\n");
+                    }
+                } finally {
+                    if (bufferedReader != null) {
+                        bufferedReader.close();
+                    }
+                    if (bufferedWriter != null) {
+                        bufferedWriter.close();
+                    }
+                }
+
+                return result.toString();
+            }
+        }
+
 
     class PutDataTask extends AsyncTask<String, Void, String> {
 
