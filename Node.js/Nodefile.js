5b0b9961ddb1b78ea8f91c3e606b90d1167ba354
mohamedvirj <56391235+mohamedvirji@users.noreply.github.com>
2020-03-27 02:06:14 +0000

    LOGIN PAGE ACTIVE ON SERVER

--- Node.js/Nodefile.js
+++ Node.js/Nodefile.js
@@ -1,3 +1,5 @@
+var crypto = require('crypto');
+var uuid = require('uuid');
 var express = require('express');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
@@ -5,6 +7,7 @@ var bodyParser = require('body-parser');
 var con = mysql.createConnection({
 host:'0.0.0.0',
 user:'root',
+password: 'mohamed123',
 database:'travelapp'
 });
 var app=express();
@@ -46,6 +49,98 @@ res.end(JSON.stringify('No data'));
 });
 
 });
+
+//PASSWORD ULTIL
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+    .toString('hex')/* CONVERT TO HEX */
+    .slice(0, length);
+};
+var sha512 = function (password,salt){
+    var hash = crypto.createHmac('sha512',salt);
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+function saltHashPassword(userPassword){
+    var salt = genRandomString(16);
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+function checkHashPassword(userPassword,salt){
+    var passwordData = sha512(userPassword,salt);
+    return passwordData;
+}
+
+
+
+app.post('/register/', (req,res, next)=>{
+    
+    var post_data = req.body; //GET POST PARAMS
+    
+    var uid = uuid.v4(); //Get UUID
+    var plaint_password = post_data.password;
+    var hash_data = saltHashPassword(plaint_password);
+    var password = hash_data.passwordHash; //Get HASH VALUE
+    var salt = hash_data.salt; //Get SALT
+    
+    var name = post_data.name;
+    var email = post_data.email;
+    
+    con.query('SELECT * FROM users where email=?', [email],function(err,result,fields){
+        con.on('error', function(err){
+            console.log('[MySQL ERROR]',err);
+        });
+        
+                if(result && result.length)
+            res.json('User already exists!!!');
+        else 
+        {
+            con.query('INSERT INTO users (unique_id, name, email, encrypted_password, salt, created_at, updated_at) VALUES (?, ?, ?, ?, ?, NOW(),NOW())',[uid,name,email,password,salt],function(err,result,fields){
+                con.on('error', function(err){
+            console.log('[MySQL ERROR]',err);
+                    res.json('Register error: ',err);
+        });
+                res.json('Register successful');
+            })
+        }
+    });
+})
+
+app.post('/login/', (req,res,next)=>{
+    var post_data = req.body;
+    
+    var user_password = post_data.password;
+    var email = post_data.email;
+    
+        con.query('SELECT * FROM users where email=?', [email],function(err,result,fields){
+        con.on('error', function(err){
+            console.log('[MySQL ERROR]',err);
+        });
+        
+                if(result && result.length)
+        {
+            var salt = result[0].salt;
+            var encrypted_password = result[0].encrypted_password;
+            var hashed_password = checkHashPassword(user_password,salt).passwordHash;
+            if(encrypted_password == hashed_password)
+                res.end(JSON.stringify(result[0]))
+            else
+                res.end(JSON.stringify('Wrong Password, Try again'));
+        }
+        else 
+        {
+       res.json('User does NOT exist!!!');
+        }
+    });
+    
+})
+
+
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
 
--- app/src/main/java/android/example/travelsuggestion/LoginScreen.java
+++ app/src/main/java/android/example/travelsuggestion/LoginScreen.java
@@ -1,22 +1,100 @@
 package android.example.travelsuggestion;
 
 import android.content.Intent;
+import android.example.travelsuggestion.Retrofit.INodeJS;
+import android.example.travelsuggestion.Retrofit.RetrofitClient;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+
 public class LoginScreen extends AppCompatActivity {
+
+    INodeJS myAPI;
+    CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    EditText edt_email, edt_password;
+    Button btn_login, btn_register;
+
+    @Override
+    protected void onStop(){
+        compositeDisposable.clear();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        compositeDisposable.clear();
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        //INIT API
+        Retrofit retrofit = RetrofitClient.getInstance();
+        myAPI = retrofit.create(INodeJS.class);
+
+        //View
+        btn_login = (Button)findViewById(R.id.login);
+        btn_register = (Button)findViewById(R.id.register);
+
+        edt_email = (EditText)findViewById(R.id.LoginUsername);
+        edt_password = (EditText)findViewById(R.id.LoginPassword);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loginUser(edt_email.getText().toString(), edt_password.getText().toString());
+            }
+        });
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser(edt_email.getText().toString(), edt_password.getText().toString());
+            }
+        });
+        
+
     }
 
-    public void Login(View view) {
+    private void registerUser(String email, String password) {
+
+
+    }
+
+    private void loginUser(String email, String password) {
+        compositeDisposable.add(myAPI.loginUser(email, password)
+        .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                    if(s.contains("encrypted_password"))
+                        Toast.makeText(LoginScreen.this, "Login Success", Toast.LENGTH_SHORT).show();
+                    else
+                        Toast.makeText(LoginScreen.this, ""+s, Toast.LENGTH_SHORT).show();
+                    }
+                })
+        );
+        }
+
+
+    /*public void Login(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-
-    }
+    }*/
 }
