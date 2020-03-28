37e283c484869560549e23929000debde5f65631
mohamedvirj <56391235+mohamedvirji@users.noreply.github.com>
2020-03-28 01:29:41 +0000

    Login and Registration Complete
    Signed-off-by: mohamedvirj <56391235+mohamedvirji@users.noreply.github.com>

--- app/src/main/java/android/example/travelsuggestion/LoginScreen.java
+++ app/src/main/java/android/example/travelsuggestion/LoginScreen.java
@@ -1,15 +1,21 @@
 package android.example.travelsuggestion;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.example.travelsuggestion.Retrofit.INodeJS;
 import android.example.travelsuggestion.Retrofit.RetrofitClient;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import io.reactivex.Scheduler;
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -18,7 +24,7 @@ import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 
-public class LoginScreen extends AppCompatActivity {
+public class LoginScreen<LoginDialogFragment> extends AppCompatActivity {
 
     INodeJS myAPI;
     CompositeDisposable compositeDisposable = new CompositeDisposable();
@@ -73,7 +79,37 @@ public class LoginScreen extends AppCompatActivity {
 
     private void registerUser(String email, String password) {
 
+        View register_user_view = LayoutInflater.from(this).inflate(R.layout.register_user, null);
 
+        new AlertDialog.Builder(this)
+                .setTitle("Register")
+                .setMessage("Please enter your details below to register")
+                .setView(register_user_view)
+                .setIcon(R.drawable.icon_padlock)
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                })
+                .setPositiveButton(R.string.register, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked Register button
+                EditText edt_name = (EditText)register_user_view.findViewById(R.id.edt_name);
+                        EditText new_email = (EditText)register_user_view.findViewById(R.id.new_email);
+                        EditText new_password = (EditText)register_user_view.findViewById(R.id.new_password);
+
+                        compositeDisposable.add(myAPI.registerUser(new_email.getText().toString(),edt_name.getText().toString(),new_password.getText().toString())
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .subscribe(new Consumer<String>() {
+                                    @Override
+                                    public void accept(String s) throws Exception {
+                                Toast.makeText(LoginScreen.this, ""+s, Toast.LENGTH_SHORT).show();
+                                    }
+                                }));
+
+            }
+        }).show();
     }
 
     private void loginUser(String email, String password) {
